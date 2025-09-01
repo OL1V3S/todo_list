31602350f60c089e33583681a319b07cc6eba142
Oliver <olivertriana7319@gmail.com>
2025-09-01 17:12:37 -0500

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# To-Do List
+
+A simple to-do list web app built with HTML, CSS, and JavaScript.
+- Add, delete, and mark tasks as completed.
+- Tasks persist using localStorage.
+- Hosted on GitHub Pages.
---
+++ script.js
@@ -0,0 +1,76 @@
+// Get references to the input, button, and task list
+const taskInput = document.getElementById("task-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const taskList = document.getElementById("task-list");
+
+// Load existing tasks from localStorage
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+// Render tasks when page loads
+renderTasks();
+
+
+// Make "add" button actually do something when clicked
+addTaskBtn.addEventListener("click", () => {
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") { // "===" checks value AND type
+        alert("No task entered >:(");
+        return;
+    }
+
+    // Add new task to array
+    tasks.push({text: taskText, completed: false});
+
+    // Save to localStorage
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+
+    // Clear input box
+    taskInput.value="";
+
+    // Re-render updated list
+    renderTasks();
+});
+
+function renderTasks() {
+    // Clear current list to not duplicate tasks
+    taskList.innerHTML = "";
+
+    // Looping through every task
+    tasks.forEach((task, index) => {
+        const li = document.createElement("li");
+        li.textContent = task.text;
+
+        // If task is completed, style different
+        if(task.completed) {
+            li.classList.add("completed");
+        }
+
+        // Complete task when clicked
+        li.addEventListener("click", () => {
+            tasks[index].completed = !tasks[index].completed;
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            renderTasks();
+        });
+
+        // Create delete button
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.classList.add("delete-btn");
+        deleteBtn.addEventListener("click", (e) => {
+            e.stopPropagation(); // Stops click from also marking task completed
+            tasks.splice(index,1); // Remove task from array
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+            renderTasks();
+        });
+
+        // Add delete button to list item
+        li.appendChild(deleteBtn); // Add inside li so it appears next to task
+
+        // Add list item to task list
+        taskList.appendChild(li);
+
+    });
+
+
+
+}
